e6369062c053240587d70c8562b9581229a517eb
nldreyer <nldreyer@ksu.edu>
2019-09-20 14:29:33 -0500

    Implemented in-class fixes.

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace DoesItBlend
 {
@@ -6,7 +8,24 @@ namespace DoesItBlend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> blendables = new List<IBlendable>();
+            blendables.Add(new Strawberry());
+            blendables.Add(new Banana());
+            blendables.Add(new CellPhone());
+            blendables.Add(new IceCubes());
+            for (int i = 0; i < 10; i++)
+            {
+                blendables.Add(new Mango());
+            }
+
+            StringBuilder mess = new StringBuilder();
+
+            foreach (IBlendable blendable in blendables)
+            {
+                mess.AppendLine(blendable.Blend());
+            }
+
+            Console.WriteLine(mess);
         }
     }
 }
